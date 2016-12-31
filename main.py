d8f7461a0bbf99a4a84821405c36f6ad85c21760
ravenusmc <mcuddy77@gmail.com>
2016-12-31 16:53:25 -0500

    working on web pages

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template
 from one import *
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return 'Hello World'
+def index():
+  return render_template('index.html', the_title='Home Page')
 
 @app.route('/entry')
 def entry_page():
   return render_template('entry.html', the_title='Mikes Math Site')
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 # @app.route('/one')
 # def first():
 #   return str(num(2,3))
 
 app.run(debug=True)
+
