21c37f903ab451843100a62ac023b8d1722de7cc
ravenusmc <mcuddy77@gmail.com>
2016-12-31 14:19:50 -0500

    added a simple function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from one import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
   return 'Hello World'
+
+@app.route('/one')
+def first():
+  return str(num(2,3))
 
 app.run()
---
+++ one.py
@@ -0,0 +1,3 @@
+def num(num1, num2):
+  answer = num1 + num2 
+  return answer
