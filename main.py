484a24d8881535c27c0b73c9c4db1aabaa358c75
ravenusmc <mcuddy77@gmail.com>
2017-01-11 11:43:56 -0500

    started on multiply pages

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
   return render_template('index.html', the_title='Home Page')
 
+#Code for addition pages
+@app.route('/entry')
+def entry_page():
+    return render_template('entry.html', the_title='Mikes Math Site')
+
 @app.route('/one', methods=['POST'])
 def first():
     numOne = int(request.form['numberOne'])
@@ -15,8 +20,12 @@ def first():
     results = str(num(numOne, numTwo))
     return render_template('results.html', numOne = numOne, numTwo = numTwo, result = results)
 
-@app.route('/entry')
-def entry_page():
-    return render_template('entry.html', the_title='Mikes Math Site')
+#Code for multiplication pages
+@app.route('/multiply')
+def multiply_page():
+    return render_template('times.html', the_title='Multiply Time!')
+
+
+#NEED TO MAKE ONE FINAL SET OF PAGES FOR USERS NAME
 
 app.run(debug=True)
